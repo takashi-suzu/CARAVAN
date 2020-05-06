268f1cf4ecb5e4ee451577538d84872e09382d1b
takashi-suzu <t.suzuki33333@gmail.com>
2020-05-06 12:34:20 +0900

    [finish]4-3

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -6,8 +6,23 @@ class BlogsController < ApplicationController
   end
 
   def new
+  	@blog = Blog.new
+  end
+
+  def create
+  	blog = Blog.new
+  	blog.save
+  	redirect_to blogs_path
   end
 
   def edit
   end
+
+
+  private
+
+  def blog_params
+  	params_require(:blog).permit(:title, :category, :body)
+  end
+
 end
