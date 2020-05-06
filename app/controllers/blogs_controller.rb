71a78215172f5d8f5ac09f7e119ad80e47fc0507
takashi-suzu <t.suzuki33333@gmail.com>
2020-05-06 14:47:53 +0900

    [finish]4-6

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -12,19 +12,25 @@ class BlogsController < ApplicationController
   end
 
   def create
-  	blog = Blog.new
+  	blog = Blog.new(blog_params)
   	blog.save
-  	redirect_to blogs_path(blog.id)
+  	redirect_to blogs_path
   end
 
   def edit
+  	@blog = Blog.find(params[:id])
+  end
+
+  def update
+  	blog = Blog.find(params[:id])
+  	blog.update(blog_params)
+  	redirect_to blog_path(blog)
   end
 
 
   private
-
   def blog_params
-  	params_require(:blog).permit(:title, :category, :body)
+  	params.require(:blog).permit(:title, :category, :body)
   end
 
 end
